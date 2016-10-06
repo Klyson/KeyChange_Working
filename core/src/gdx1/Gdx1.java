89cfe751510e413e3da554807039f1c2bf38611e
weild5554 <weild5554@FHC-IC0034102.wrdsb.ca>
2016-10-06 13:04:57 -0400

    Sprite Movement Within Methods (GamesFromScratch lesson mouse and keyboard input).

--- core/src/gdx1/Gdx1.java
+++ core/src/gdx1/Gdx1.java
@@ -1,31 +1,112 @@
 package gdx1;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Buttons;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class Gdx1 extends ApplicationAdapter {
-	SpriteBatch asd;
-	Texture img;
-        Sprite sprite;
-	
-	@Override
-	public void create () {
-		asd = new SpriteBatch();
-		img = new Texture("CoreSprite.png");
-                sprite = new Sprite(img);
-                sprite.setPosition(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
-               }
+public class Gdx1 implements ApplicationListener, InputProcessor {
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(0, 0, 0, 0);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		asd.begin();
-		asd.draw(img, sprite.getX(), sprite.getY(), Gdx.graphics.getWidth()/5 , Gdx.graphics.getHeight()/5);
-		asd.end();
-	}
+    private SpriteBatch batch;
+    private Texture texture;
+    private Sprite sprite;
+    private float posX, posY;
+
+    @Override
+    public void create() {
+        float w = Gdx.graphics.getWidth();
+        float h = Gdx.graphics.getHeight();
+        batch = new SpriteBatch();
+
+        texture = new Texture(Gdx.files.internal("CoreSprite.png"));
+        sprite = new Sprite(texture);
+        posX = w / 2 - sprite.getWidth() / 2;
+        posY = h / 2 - sprite.getHeight() / 2;
+        sprite.setPosition(posX, posY);
+
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        texture.dispose();
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        sprite.setPosition(posX, posY);
+        batch.begin();
+        sprite.draw(batch);
+        batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        if (keycode == Keys.SPACE) {
+            sprite.setPosition(posX, posY);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        if (keycode == Keys.LEFT) {
+            posX += 5;
+        }
+        if (keycode == Keys.RIGHT) {
+            posX -= 5;
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
+    }
 }
