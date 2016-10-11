d029c835780d1f5f34b685fb52cb96839af75c8f
weild5554 <weild5554@FHC-IC0034102.wrdsb.ca>
2016-10-11 14:19:24 -0400

    Sprite Movement Booleans

--- core/src/gdx1/Gdx1.java
+++ core/src/gdx1/Gdx1.java
@@ -2,20 +2,19 @@ package gdx1;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input.Buttons;
 import com.badlogic.gdx.Input.Keys;
-import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class Gdx1 implements ApplicationListener, InputProcessor {
+public class Gdx1 extends InputAdapter implements ApplicationListener {
 
     private SpriteBatch batch;
     private Texture texture;
     private Sprite sprite;
-    private float posX, posY;
+    private boolean isLeft, isRight;
 
     @Override
     public void create() {
@@ -25,9 +24,9 @@ public class Gdx1 implements ApplicationListener, InputProcessor {
 
         texture = new Texture(Gdx.files.internal("CoreSprite.png"));
         sprite = new Sprite(texture);
-        posX = w / 2 - sprite.getWidth() / 2;
-        posY = h / 2 - sprite.getHeight() / 2;
-        sprite.setPosition(posX, posY);
+        sprite.setPosition(w / 2 - sprite.getWidth() / 2, h / 2 - 
+                sprite.getHeight() / 2);
+        Gdx.input.setInputProcessor(this);
 
     }
 
@@ -41,8 +40,11 @@ public class Gdx1 implements ApplicationListener, InputProcessor {
     public void render() {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        sprite.setPosition(posX, posY);
+        if (isLeft) {
+            sprite.translateX(-1f);
+        } if (isRight) {
+            sprite.translateX(1f);
+        }
         batch.begin();
         sprite.draw(batch);
         batch.end();
@@ -62,8 +64,11 @@ public class Gdx1 implements ApplicationListener, InputProcessor {
 
     @Override
     public boolean keyDown(int keycode) {
-        if (keycode == Keys.SPACE) {
-            sprite.setPosition(posX, posY);
+        if (keycode == Keys.LEFT) {
+            isLeft = true;
+        }
+        if (keycode == Keys.RIGHT) {
+            isRight = true;
         }
         return true;
     }
@@ -71,42 +76,11 @@ public class Gdx1 implements ApplicationListener, InputProcessor {
     @Override
     public boolean keyUp(int keycode) {
         if (keycode == Keys.LEFT) {
-            posX += 5;
+            isLeft = false;
         }
         if (keycode == Keys.RIGHT) {
-            posX -= 5;
+            isRight = false;
         }
-
         return true;
-    }
-
-    @Override
-    public boolean keyTyped(char character) {
-        return false;
-    }
-
-    @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        return false;
-    }
-
-    @Override
-    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        return false;
-    }
-
-    @Override
-    public boolean touchDragged(int screenX, int screenY, int pointer) {
-        return false;
-    }
-
-    @Override
-    public boolean mouseMoved(int screenX, int screenY) {
-        return false;
-    }
-
-    @Override
-    public boolean scrolled(int amount) {
-        return false;
     }
 }
